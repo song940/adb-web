89fb8786e43b8a377467be58130c07ee34d49d9c
Lsong <song940@gmail.com>
2022-12-15 17:58:48 +0800

    update

--- src/common/TypedEmitter.ts
+++ src/common/TypedEmitter.ts
@@ -5,7 +5,7 @@ export type EventMap = Record<string, any>;
 export type EventKey<T extends EventMap> = string & keyof T;
 export type EventReceiver<T> = (params: T) => void;
 
-interface Emitter<T extends EventMap> {
+export interface Emitter<T extends EventMap> {
     on<K extends EventKey<T>>(eventName: K, fn: EventReceiver<T[K]>): void;
     off<K extends EventKey<T>>(eventName: K, fn: EventReceiver<T[K]>): void;
     emit<K extends EventKey<T>>(eventName: K, params: T[K]): void;
